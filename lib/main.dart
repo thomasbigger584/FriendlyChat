e7ab8ae352ea8488c2aaa3cf1d2be2fdb0234202
Thomas Bigger <thomas@performanceactive.com>
2018-04-18 12:40:27 +0100

    fade in animation and size change animation on chat message add to list

--- lib/main.dart
+++ lib/main.dart
@@ -81,7 +81,7 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   void _handleSubmitted(String text) {
     _textEditingController.clear();
     AnimationController animationController = new AnimationController(
-      duration: new Duration(milliseconds: 500),
+      duration: new Duration(milliseconds: 700),
       vsync: this,
     );
     animationController.addStatusListener(_handleAnimationStatus);
@@ -120,41 +120,49 @@ class ChatMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Animation<double> opacityAnimation = new Tween<double>(
-      begin: 0.0,
-      end: 1.0,
-    ).animate(animationController);
-    return new FadeTransition(
-      opacity: opacityAnimation,
-      child: new Container(
-        margin: const EdgeInsets.symmetric(vertical: 10.0),
-        child: new Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            new Container(
-              margin: const EdgeInsets.only(right: 16.0),
-              child: new InkWell(
-                child: new CircleAvatar(
-                  child: new Text(_name[0]),
+    return new SizeTransition(
+      sizeFactor: new CurvedAnimation(
+          parent: animationController, curve: Curves.fastOutSlowIn),
+      axisAlignment: 0.0,
+      child: new FadeTransition(
+        opacity: _buildOpacityAnimation(),
+        child: new Container(
+          margin: const EdgeInsets.symmetric(vertical: 10.0),
+          child: new Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(right: 16.0),
+                child: new InkWell(
+                  child: new CircleAvatar(
+                    child: new Text(_name[0]),
+                  ),
+                  onTap: _handleAvatarPress,
+                  onLongPress: _handleAvatarLongPress,
                 ),
-                onTap: _handleAvatarPress,
-                onLongPress: _handleAvatarLongPress,
               ),
-            ),
-            new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                new Text(_name, style: Theme.of(context).textTheme.subhead),
-                new Container(
-                  margin: const EdgeInsets.only(top: 5.0),
-                  child: new Text(text),
-                ),
-              ],
-            ),
-          ],
+              new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text(_name, style: Theme.of(context).textTheme.subhead),
+                  new Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: new Text(text),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Animation<double> _buildOpacityAnimation() {
+    return new Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(animationController);
   }
 
   void _handleAvatarPress() {
