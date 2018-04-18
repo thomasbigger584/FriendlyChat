206913a4740108f9283646e74f703de447e47a66
Thomas Bigger <thomas@performanceactive.com>
2018-04-18 13:02:32 +0100

    some styling to add cards to the chat message for aesthetics

--- lib/main.dart
+++ lib/main.dart
@@ -34,13 +34,16 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
         children: <Widget>[
           new Flexible(
             child: new ListView.builder(
-              padding: new EdgeInsets.all(8.0),
+              padding: new EdgeInsets.all(4.0),
               reverse: true,
               itemBuilder: (_, int index) => _messages[index],
               itemCount: _messages.length,
             ),
           ),
-          new Divider(),
+          new Divider(
+            height: 8.0,
+            indent: 8.0,
+          ),
           new Container(
             decoration: new BoxDecoration(color: Theme.of(context).cardColor),
             child: _buildTextComposer(),
@@ -54,7 +57,7 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
     return new IconTheme(
       data: new IconThemeData(color: Theme.of(context).accentColor),
       child: new Container(
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
         child: new Row(
           children: <Widget>[
             new Flexible(
@@ -123,35 +126,36 @@ class ChatMessage extends StatelessWidget {
     return new SizeTransition(
       sizeFactor: new CurvedAnimation(
           parent: animationController, curve: Curves.fastOutSlowIn),
-      axisAlignment: 0.0,
       child: new FadeTransition(
         opacity: _buildOpacityAnimation(),
-        child: new Container(
-          margin: const EdgeInsets.symmetric(vertical: 10.0),
-          child: new Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.only(right: 16.0),
-                child: new InkWell(
-                  child: new CircleAvatar(
-                    child: new Text(_name[0]),
+        child: new Card(
+          child: new Container(
+            margin: const EdgeInsets.all(8.0),
+            child: new Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(right: 16.0),
+                  child: new InkWell(
+                    child: new CircleAvatar(
+                      child: new Text(_name[0]),
+                    ),
+                    onTap: _handleAvatarPress,
+                    onLongPress: _handleAvatarLongPress,
                   ),
-                  onTap: _handleAvatarPress,
-                  onLongPress: _handleAvatarLongPress,
                 ),
-              ),
-              new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new Text(_name, style: Theme.of(context).textTheme.subhead),
-                  new Container(
-                    margin: const EdgeInsets.only(top: 5.0),
-                    child: new Text(text),
-                  ),
-                ],
-              ),
-            ],
+                new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new Text(_name, style: Theme.of(context).textTheme.subhead),
+                    new Container(
+                      margin: const EdgeInsets.only(top: 5.0),
+                      child: new Text(text),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
